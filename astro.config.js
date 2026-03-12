3dc8774e96cc75143fec48072b65e66a2a8c2ca1
Xi Zhang <tcdw2011@gmail.com>
2026-03-12 20:29:17 +0800

    Initial commit

---
+++ astro.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  // 请修改为你自己的线上地址，谢谢茄子
+  site: 'https://tcdw.github.io/koi',
+
+  // 如果你的网站在子路径下（例如 `https://example.com/koi/`），则填写 `/koi/`
+  // 在根路径下（例如 `https://example.com/`）则填写 `/`
+  base: process.env.NODE_ENV === "production" ? "/koi/" : "",
+
+  integrations: [mdx(), sitemap(), svelte()],
+
+  markdown: {
+    remarkRehype: {
+      footnoteLabel: "脚注",
+      footnoteBackLabel: '文档内容的脚注',
+    }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
