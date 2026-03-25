eaaea6107ac2f303cbaad2c96419cc1658487ef3
tcdw <tcdw2011@gmail.com>
2026-03-25 21:55:07 +0800

    feat: add images

--- src/data/facts.ts
+++ src/data/facts.ts
@@ -1,5 +1,9 @@
 import hobbyMusic from "@/assets/hobbies/hobby_music.jpg";
 import hobbyCity from "@/assets/hobbies/hobby_city.jpg";
+import hobbyCafeSearch from "@/assets/hobbies/hobby_cafe_search.jpg";
+import hobbyAbstract from "@/assets/hobbies/hobby_abstract.jpg";
+import hobbyGoods from "@/assets/hobbies/hobby_goods.jpg";
+import favoriteNayukiTea from "@/assets/hobbies/favorite_nayuki_tea.jpg";
 
 export interface Fact {
   image: ImageMetadata;
@@ -17,5 +21,25 @@ export const FACTS: Fact[] = [
     image: hobbyCity,
     alt: "摄影",
     text: "tcdw 会随手拍下奇怪的城市角落。",
+  },
+  {
+    image: hobbyCafeSearch,
+    alt: "咖啡店探店",
+    text: "tcdw 喜欢在去不同的咖啡店坐坐，工作日的晚上也是。",
+  },
+  {
+    image: hobbyAbstract,
+    alt: "抽象艺术",
+    text: "tcdw 喜欢搞抽象，即使它们毫无实际价值。",
+  },
+  {
+    image: hobbyGoods,
+    alt: "收藏品",
+    text: "tcdw 喜欢收集各种谷子（小物件），尤其是《蔚蓝档案》的同人谷。",
+  },
+  {
+    image: favoriteNayukiTea,
+    alt: "奈雪的茶",
+    text: "tcdw 喜欢喝奈雪的茶。",
   },
 ];
